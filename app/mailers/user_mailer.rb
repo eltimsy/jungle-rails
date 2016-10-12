a1b2b41bc61e3c8e03844203b3017a7ee6c35059
Timothy Tse <TimmyC@Timothys-MacBook-Air.local>
2016-10-12 10:29:18 -0400

    Can send test emails for new users and after user orders

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,10 @@ class UserMailer < ApplicationMailer
     @user = user
     @url = '/login'
     mail(to: @user.email, subject: "Welcome")
+  end
+
+  def order_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Your Order #{@order.id}")
+  end
 end
