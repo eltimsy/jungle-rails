96c4ce688f1bd1f0fb8684bb366b3c337c25e021
Timothy Tse <timothy.sc.tse@gmail.com>
2016-10-17 13:11:31 -0400

    quantity is now properly subtracted from database

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,25 +2,20 @@ class Order < ActiveRecord::Base
 
   belongs_to :user
   has_many :line_items
-  after_create :change_quantity
+  after_save :change_quantity
 
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
 
-  # private
-  #   def change_quantity
-  #     order_items = LineItem.where(order_id: id)
-  #     order_items.each do |item|
-  #       puts "alsdfjlaskdjflasdkjflasdkjflasdkjflasdkjfalsdkfj"
-  #       puts total_cents
-  #       puts item.quantity
-  #       puts item.product.quantity
-  #       item.product.quantity -= item.quantity
-  #       puts item.product.quantity
-  #     end
-  #
-  #   end
+  private
+    def change_quantity
+      order_items = LineItem.where(order_id: id)
+      order_items.each do |item|
+        item.product.quantity -= item.quantity
+        item.product.save!
+      end
+    end
 end
 #
 # create_table "orders", force: :cascade do |t|
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pp'
 
 RSpec.describe Order, type: :model do
   describe 'After creation' do
@@ -26,40 +27,80 @@ RSpec.describe Order, type: :model do
     it 'deducts quantity from products based on their line item quantities' do
       # TODO: Implement based on hints below
       # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
-      @order = Order.new(
+
+      @order = Order.create!(
       total_cents: 20000,
       email: "abc@abc.com",
       stripe_charge_id: "123kdsjf"
       )
       # 2. build line items on @order
-      @line_item1 = LineItem.create!(
-      order_id: @order,
-      product_id: @product1,
+      @line_item1 = LineItem.new(
+      order_id: @order.id,
+      product_id: @product1.id,
       quantity: 1,
       item_price_cents: @product1.price,
       total_price_cents: @product1.price * 1
       )
-      @line_item2 = LineItem.create!(
-      order_id: @order,
-      product_id: @product2,
+
+      @line_item2 = LineItem.new(
+      order_id: @order.id,
+      product_id: @product2.id,
       quantity: 1,
       item_price_cents: @product2.price,
       total_price_cents: @product2.price * 1
       )
+
+      @line_item1.save!
+      @line_item2.save!
+      @order.save!
       # ...
       # 3. save! the order - ie raise an exception if it fails (not expected)
-      @order.save!
+
       # 4. reload products to have their updated quantities
       @product1.reload
       @product2.reload
       expect(@product1[:quantity]).to eql(19)
-      expect(@product2[:qauntity]).to eql(49)
+      expect(@product2[:quantity]).to eql(49)
       # 5. use RSpec expect syntax to assert their new quantity values
       # ...
     end
     # pending test 2
-    xit 'does not deduct quantity from products that are not in the order' do
+    it 'does not deduct quantity from products that are not in the order' do
       # TODO: Implement based on hints in previous test
+      @order = Order.create!(
+      total_cents: 20000,
+      email: "abc@abc.com",
+      stripe_charge_id: "123kdsjf"
+      )
+      # 2. build line items on @order
+      @line_item1 = LineItem.new(
+      order_id: @order.id,
+      product_id: @product1.id,
+      quantity: 1,
+      item_price_cents: @product1.price,
+      total_price_cents: @product1.price * 1
+      )
+
+      @line_item2 = LineItem.new(
+      order_id: @order.id,
+      product_id: @product2.id,
+      quantity: 1,
+      item_price_cents: @product2.price,
+      total_price_cents: @product2.price * 1
+      )
+
+      @line_item1.save!
+      @line_item2.save!
+      @order.save!
+      # ...
+      # 3. save! the order - ie raise an exception if it fails (not expected)
+
+      # 4. reload products to have their updated quantities
+      @product1.reload
+      @product2.reload
+      @product3.reload
+      expect(@product3[:quantity]).to eql(10)
+
     end
   end
 end
